32310ab036b5f853bce1b6b2cdd0146889a8c1da
A Mon <a.mont.24.777@gmail.com>
2023-11-22 17:33:51 -0600

    Update

--- Gear-Template-Frontend/src/pages/home/Home.tsx
+++ Gear-Template-Frontend/src/pages/home/Home.tsx
@@ -1,11 +1,49 @@
-
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Center,
+  HStack,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
+import { Mint } from "./MintButton";
+import { LocalBalanceToken } from "./MyBalanceToken";
+import { Burn } from "./Burn";
+import { Transfer } from "./Transfer";
+import { NameFungibleToken } from "./NameToken";
 
 function Home() {
- 
-
   return (
-    <h1>Hola Mundo</h1>
-  )
+    <Center>
+      <Card w="300px" h="600px">
+        <CardHeader>
+          <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png" />
+        </CardHeader>
+        <CardBody>
+          <Center>
+            <Heading>
+              Token:
+              <NameFungibleToken />
+            </Heading>
+          </Center>
+          <Center>
+            <LocalBalanceToken />
+          </Center>
+        </CardBody>
+        <CardFooter>
+          <Center>
+            <HStack>
+              <Mint />
+              <Burn />
+              <Transfer />
+            </HStack>
+          </Center>
+        </CardFooter>
+      </Card>
+    </Center>
+  );
 }
 
 export { Home };
