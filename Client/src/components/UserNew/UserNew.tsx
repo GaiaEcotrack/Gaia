8791fcde8a7f6d2b4d9583f90fc18142983f4131
AlexdrCastillo <alexdrcastillo9gmail.com>
2023-12-09 09:00:44 -0400

    commit

--- Client/src/components/UserNew/UserNew.tsx
+++ Client/src/components/UserNew/UserNew.tsx
@@ -1,32 +1,32 @@
 
 function PublicProfile () {
   return (
-    <div className="bg-white w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-[#161931]">
+    <div className=" w-full flex flex-col gap-5 px-3 md:px-16 lg:px-28 md:flex-row text-white">
       {/* Aside */}
       <aside className="hidden py-4 md:w-1/3 lg:w-1/4 md:block">
         <div className="sticky flex flex-col gap-2 p-4 text-sm border-r border-indigo-100 top-12">
           <h2 className="pl-3 mb-4 text-2xl font-semibold">Settings</h2>
           <a
             href="/profile"
-            className="flex items-center px-3 py-2.5 font-bold bg-white text-indigo-900 border rounded-full"
+            className="flex items-center px-3 py-2.5 font-bold bg-white text-black border rounded-full"
           >
             Public Profile
           </a>
           <a
             href="/settings"
-            className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
+            className="flex items-center px-3 py-2.5 font-semibold hover:text-white hover:border hover:rounded-full"
           >
             Account Settings
           </a>
           <a
             href="/NotificacionesConfig"
-            className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
+            className="flex items-center px-3 py-2.5 font-semibold hover:text-white hover:border hover:rounded-full"
           >
             Notifications
           </a>
           <a
             href="/account"
-            className="flex items-center px-3 py-2.5 font-semibold hover:text-indigo-900 hover:border hover:rounded-full"
+            className="flex items-center px-3 py-2.5 font-semibold hover:text-white hover:border hover:rounded-full"
           >
             PRO Account
           </a>
