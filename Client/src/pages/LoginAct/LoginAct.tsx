f6d3b4b98401f4f133f6083fc1082bf4d256fae2
diego34346 <diego_f423@hotmail.com>
2023-12-12 05:26:53 -0500

    README

--- Client/src/pages/LoginAct/LoginAct.tsx
+++ Client/src/pages/LoginAct/LoginAct.tsx
@@ -46,6 +46,7 @@ function AuthForm (props: ILoginPageProps): JSX.Element {
     }
   };
 
+  
     const imageUrl = 'https://images.unsplash.com/photo-1467533003447-e295ff1b0435?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
   return (
@@ -74,7 +75,7 @@ function AuthForm (props: ILoginPageProps): JSX.Element {
                 id="email"
                 name="email"
                 placeholder="Enter Email Address"
-                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                className="w-full px-4 py-3 rounded-lg text-black bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                 autoComplete="on"
                 required
               />
@@ -90,25 +91,28 @@ function AuthForm (props: ILoginPageProps): JSX.Element {
                 name="password"
                 placeholder="Enter Password"
                 minLength={6}
-                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+                className="w-full px-4 py-3 rounded-lg text-black bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
                 required
               />
             </div>
 
             <div className="text-right gap-5 mt-2">
-              <Link
-                to="/Forget"
+              <button
+                // to="/Forget"
                 className="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700"
+                disabled={true}
               >
                 Forgot Password?
-              </Link>
+              </button>
             </div>
 
             <button
               type="submit"
               className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg px-4 py-3 mt-6"
+              disabled={true}
             >
               Log In
+              
             </button>
           </form>
 
@@ -174,9 +178,9 @@ function AuthForm (props: ILoginPageProps): JSX.Element {
             </div>
           </button>
 
-          <Link to="/register" className="text-blue-500 hover:text-blue-700 font-semibold">
+          <button type="button" disabled={true} className="text-blue-500 hover:text-blue-700 font-semibold">
             Create an account
-          </Link>
+          </button>
           </div>
         </div>
       </div>
--- README.md
+++ README.md
@@ -1,6 +1,48 @@
+<div>
+  <img src="./Client/public/gaiaReadme.png" width = "100%">
+</div>
 
-# Gear Template for Dapps 
+# GAIA ECO-TRACK 
+
+## Start, Clone the repository:
+
+**Command:**
+```bash
+git clone https://github.com/ilichb/VaraEnergy.git
+```
+
+##  To run the Frontend, positioned in the directory /Client
+
+### STEP 1, Install dependencies:
+**Command:**
+```bash
+npm install --force
+```
+
+### STEP 2, Run the project:
+**Command:**
+```bash
+npm start
+```
+
+##  To run the Smart Contract, positioned in the directory /Contracts
+
+### PASO 1, Compile
+**Command:**
+```bash
+cargo build --release
+```
+
+### PASO 2, Deploy the contract on the IDEA platform and interact with your contract.
 
 
-## Description
+# DESCRIPTION
 
+Gaia Ecotrack seeks to integrate decentralized electricity generation systems, such as rooftop solar, domestic wind turbines and small-scale hydroelectric systems. The platform allows users to generate and sell their own energy through tokens on the blockchain. It uses optimization algorithms to coordinate the consumption and production of renewable energy in real time, adjusting demand based on solar production predictions. Mass adoption is encouraged through smart contracts developed with the GEAR protocol technology that facilitate the direct marketing of renewable energy between producers and consumers, eliminating intermediaries and reducing costs
+
+## Our team
+
+- [Agustin Nazer](https://github.com/AgusNazer)
+- [Diego Rosas](https://github.com/diego34346)
+- [Nicolas Villagra](https://github.com/NicolasVillagra)
+- [Alexander]()
