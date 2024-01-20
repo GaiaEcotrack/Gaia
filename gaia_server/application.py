9baafe6dac7e3c1d65dbca809ca3c66cdcb9b4be
AgusNazer <agustin.nazer@hotmail.com>
2024-01-19 22:26:51 -0300

    modulization routes

--- gaia_server/application.py
+++ gaia_server/application.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from flask_swagger_ui import get_swaggerui_blueprint
-from src.routes.users import user_routes
-from src.models.user import UserSchema
+# from src.routes.users import user_routes
+# from src.models.user import UserSchema
 import os
 from dotenv import load_dotenv
+# from src.routes.users import users_blueprint
+from src.routes.users import users_route
 
 load_dotenv()
 
@@ -22,6 +24,9 @@ SWAGGER_BLUEPRINT = get_swaggerui_blueprint(
 )
 
 application.register_blueprint(SWAGGER_BLUEPRINT, url_prefix=SWAGGER_URL)
+# Si deseas que la ruta POST /users esté disponible como tal, la definición
+# en el Blueprint debe ser @users_route.route('/', methods=['POST']).
+application.register_blueprint(users_route, url_prefix='/users')
 
 mongo_uri = os.getenv("MONGO_URI")
 # mongo_uri = 'mongodb+srv://gaiamongodb:84AmBlbGBNRr5CBJ@cluster0.wio4vv2.mongodb.net/?retryWrites=true&w=majority'
@@ -36,66 +41,6 @@ collection = db['users']
 def welcome():
     return jsonify({'message': 'Welcome to the Gaia Server!'})
 
-# Ruta principal para traer usuarios
-@application.route('/users', methods=['GET'])
-def get_users():
-    users = list(collection.find())
-    for user in users:
-        user['_id'] = str(user['_id'])
-    return jsonify({'message': 'Hello, Flask and MongoDB Atlas!', 'users': users})
-
-@application.route('/users', methods=['POST'])
-def add_user():
-    data = request.json
-
-    user_schema = UserSchema()
-    errors = user_schema.validate(data)
-
-
-    if errors:
-        return jsonify({'message': 'Errores de validación', 'errors': errors}), 400
-
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
-
-    # Obtener los nuevos campos
-    nombre_apellidos = data.get('nombre_apellidos')
-    numero_identificacion = data.get('numero_identificacion')
-    direccion = data.get('direccion')
-    telefono = data.get('telefono')
-    correo_electronico = data.get('correo_electronico')
-
-    documento_identidad = data.get('documento_identidad')
-    certificado_nacimiento = data.get('certificado_nacimiento')
-    certificado_matrimonio = data.get('certificado_matrimonio')
-
-    estado_cuenta_bancario = data.get('estado_cuenta_bancario')
-    declaraciones_impuestos = data.get('declaraciones_impuestos')
-    otros_documentos_financieros = data.get('otros_documentos_financieros')
-
-    # Insertar el nuevo usuario en la colección
-    new_user = {
-        'username': username,
-        'email': email,
-        'password': password,
-        'nombre_apellidos': nombre_apellidos,
-        'numero_identificacion': numero_identificacion,
-        'direccion': direccion,
-        'telefono': telefono,
-        'correo_electronico': correo_electronico,
-        'documento_identidad': documento_identidad,
-        'certificado_nacimiento': certificado_nacimiento,
-        'certificado_matrimonio': certificado_matrimonio,
-        'estado_cuenta_bancario': estado_cuenta_bancario,
-        'declaraciones_impuestos': declaraciones_impuestos,
-        'otros_documentos_financieros': otros_documentos_financieros
-    }
-
-    result = collection.insert_one(new_user)
-    inserted_id = result.inserted_id
-
-    return jsonify({'message': 'Usuario agregado con éxito', 'user_id': str(inserted_id)})
 
 # Cierra la conexión al finalizar
 @application.teardown_appcontext
--- gaia_server/src/routes/users.py
+++ gaia_server/src/routes/users.py
@@ -1,18 +1,18 @@
 
-# Bluepirnt 
-# permite organizar y estructurar tu aplicación de manera modular. Puedes pensar en un Blueprint como un conjunto de rutas, 
-# controladores y recursos que se pueden registrar y luego incorporar en tu aplicación principal.
-# Cuando importas Blueprint desde Flask, puedes crear instancias de esta clase para definir grupos lógicos de rutas y 
-# funciones de vista. Estos blueprints se pueden registrar en tu aplicación principal posteriormente.
-
-from flask import Blueprint, jsonify, request
+from flask import Flask, Blueprint, jsonify, request
 from pymongo import MongoClient
-from bson import ObjectId
+from src.models.user import UserSchema
+import os
+from dotenv import load_dotenv
 
-user_routes = Blueprint('user_routes', __name__)
+load_dotenv()
 
-# Conexión a MongoDB y configuración de la colección 'users' (código omitido por brevedad)
-mongo_uri = 'mongodb+srv://gaiamongodb:84AmBlbGBNRr5CBJ@cluster0.wio4vv2.mongodb.net/?retryWrites=true&w=majority'
+application = Flask(__name__)
+
+users_route = Blueprint('users_route', __name__)
+
+# Obtener la URI de MongoDB de las variables de entorno
+mongo_uri = os.getenv("MONGO_URI")
 client = MongoClient(mongo_uri, tlsAllowInvalidCertificates=True)
 if client:
     print('Db connected')
@@ -20,29 +20,66 @@ if client:
 db = client['gaia']
 collection = db['users']
 
-@user_routes.route('/users', methods=['POST'])
+# RUTEO
+@users_route.route('/', methods=['GET'])
+def get_users():
+    users = list(collection.find())
+    for user in users:
+        user['_id'] = str(user['_id'])
+    return jsonify({'message': 'Hello, Flask and MongoDB Atlas!', 'users': users})
+
+@users_route.route('/', methods=['POST'])
 def add_user():
-    try:
-        # Obtener datos del cuerpo de la solicitud
-        data = request.get_json()
+    data = request.json
 
-        # Verificar si los campos necesarios están presentes
-        required_fields = ['username', 'email', 'password']
-        if not all(field in data for field in required_fields):
-            return jsonify({'error': 'Campos faltantes en la solicitud'}), 400
+    user_schema = UserSchema()
+    errors = user_schema.validate(data)
 
-        # Crear un nuevo usuario
-        new_user = {
-            'username': data['username'],
-            'email': data['email'],
-            'password': data['password']
-        }
 
-        # Insertar el nuevo usuario en la base de datos
-        result = collection.insert_one(new_user)
-        inserted_id = result.inserted_id
+    if errors:
+        return jsonify({'message': 'Errores de validación', 'errors': errors}), 400
 
-        return jsonify({'message': 'Usuario agregado correctamente!', 'user_id': str(inserted_id)}), 201
-    except Exception as e:
-        print(f"Error al insertar el usuario: {e}")
-        return jsonify({'error': str(e)}), 500
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+    # Obtener los nuevos campos
+    nombre_apellidos = data.get('nombre_apellidos')
+    numero_identificacion = data.get('numero_identificacion')
+    direccion = data.get('direccion')
+    telefono = data.get('telefono')
+    correo_electronico = data.get('correo_electronico')
+
+    documento_identidad = data.get('documento_identidad')
+    certificado_nacimiento = data.get('certificado_nacimiento')
+    certificado_matrimonio = data.get('certificado_matrimonio')
+
+    estado_cuenta_bancario = data.get('estado_cuenta_bancario')
+    declaraciones_impuestos = data.get('declaraciones_impuestos')
+    otros_documentos_financieros = data.get('otros_documentos_financieros')
+
+    # Insertar el nuevo usuario en la colección
+    new_user = {
+        'username': username,
+        'email': email,
+        'password': password,
+        'nombre_apellidos': nombre_apellidos,
+        'numero_identificacion': numero_identificacion,
+        'direccion': direccion,
+        'telefono': telefono,
+        'correo_electronico': correo_electronico,
+        'documento_identidad': documento_identidad,
+        'certificado_nacimiento': certificado_nacimiento,
+        'certificado_matrimonio': certificado_matrimonio,
+        'estado_cuenta_bancario': estado_cuenta_bancario,
+        'declaraciones_impuestos': declaraciones_impuestos,
+        'otros_documentos_financieros': otros_documentos_financieros
+    }
+
+    result = collection.insert_one(new_user)
+    inserted_id = result.inserted_id
+
+    return jsonify({'message': 'Usuario agregado con éxito', 'user_id': str(inserted_id)})
+
+if __name__ == '__main__':
+    application.run(debug=True)
