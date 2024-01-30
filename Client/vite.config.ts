a8d04f840a0e02611522c9c487f55268b1aa89b9
JoseNicolas <villagrajosenicolas@gmail.com>
2024-01-30 14:39:31 -0300

    update

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -12,11 +12,6 @@ dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   server: {
     host: '0.0.0.0',
     port: 3000,
