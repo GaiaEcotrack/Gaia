75eb55363cc3e6a26472d1da76abd5348c144d2d
agusNazer <agustin.nazer@hotmail.com>
2024-03-01 12:11:28 -0300

    chatbot

--- gaia_server/application.py
+++ gaia_server/application.py
@@ -17,6 +17,7 @@ from src.routes.coinbase import coinbase_route
 from src.routes.sms import sms_route
 from src.routes.paypal import paypal_route
 from src.routes.authenticator import auth_route
+from src.routes.chatbox import chatbox_route
 
 
 
@@ -50,6 +51,7 @@ application.register_blueprint(coinbase_route, url_prefix='/coinbase')
 application.register_blueprint(sms_route, url_prefix='/sms')
 application.register_blueprint(paypal_route, url_prefix='/paypal')
 application.register_blueprint(auth_route, url_prefix='/auth')
+application.register_blueprint(chatbox_route, url_prefix='/chatbox')
 
 mongo_uri = os.getenv("MONGO_URI")
 client = MongoClient(mongo_uri, tlsAllowInvalidCertificates=True)
@@ -111,10 +113,12 @@ collection = db['users']
 def welcome():
     return jsonify({'message': 'Welcome to the Gaia Server!'})
 
+# if __name__ == '__main__':
+#     try:
+#         application.run(debug=False)
+#     finally:
+#         client.close()
+        
+#durante desarrollo
 if __name__ == '__main__':
-    try:
-        application.run(debug=True)
-    finally:
-        # No es necesario cerrar explícitamente el cliente de MongoDB aquí
-        # client.close()
-        pass
+    application.run(debug=True)
