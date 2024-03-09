07fb640b11c7d5ff8e486ff9314c5ea43ad3496f
agusNazer <agustin.nazer@hotmail.com>
2024-03-08 21:03:57 -0300

    auth panel usuairo final

--- Client/src/pages/panelUsuarioFinal/PanelUsuarioFinal.tsx
+++ Client/src/pages/panelUsuarioFinal/PanelUsuarioFinal.tsx
@@ -1,18 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
-// import { MdExpandLess, MdExpandMore } from "react-icons/md";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 
-// import Update from "../../assets/Update.svg";
-// import back from "../../assets/back.svg";
 import arrow from "../../assets/arrow.png";
 
 import { ApiLoader } from "../../components/loaders/api-loader/ApiLoader";
 import { IoIosAddCircle } from "react-icons/io";
 import { getAuth } from "firebase/auth";
-
-
+import authback from "@/apiauth/authback";
 
 // Definición de la interfaz Dispositivo
 interface Dispositivo {
@@ -37,18 +33,16 @@ const PanelUsuarioFinal = () => {
   const [selectedDevice, setSelectedDevice] = useState<Dispositivo | null>(
     null
   );
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
+  const [userError, setUserError] = useState(false);
 
   useEffect(() => {
-    // Obtener el valor almacenado en localStorage cuando el componente se monta
-    const datoAlmacenado = window.localStorage.getItem('name');
+    const datoAlmacenado = window.localStorage.getItem("name");
 
-    // Establecer el estado con el valor de localStorage, si existe
     if (datoAlmacenado) {
       setName(datoAlmacenado);
     }
-  }, []); // El segundo parámetro [] asegura que useEffect solo se ejecute una vez al montar el componente
-
+  }, []);
 
   const [typeEffect] = useTypewriter({
     words: [`Hello, ${name}!, Here you can check out your conections!⚙️`],
@@ -58,73 +52,37 @@ const PanelUsuarioFinal = () => {
   });
 
   useEffect(() => {
-    const auth = getAuth();
-    // Escucha los cambios en el estado de autenticación
-    const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      if (user) {
-        setIsLoading(true);
-        try {
-          const idToken = await user.getIdToken();
-          const apiUrl = import.meta.env.VITE_APP_API_URL;
-          const url = `${apiUrl}/devices/plant-devices?plantId=13`;
-  
-          const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-              "Authorization": `Bearer ${idToken}`,
-              "Content-Type": "application/json"
-            },
-          });
-  
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-  
-          const data = await response.json();
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const apiUrl = import.meta.env.VITE_APP_API_URL;
+        const url = `${apiUrl}/devices/plant-devices?plantId=13`;
+        const data = await authback(url);
+        if (data.devices) {
           setDevices(data.devices);
-        } catch (error) {
-          console.error(error);
-        } finally {
-          setIsLoading(false);
+        } else {
+          throw new Error("No user");
         }
+      } catch (error) {
+        console.error(error);
+        setUserError(true);
+      } finally {
+        setIsLoading(false);
       }
-    });
-  
-    // Limpieza al desmontar el componente
-    return () => unsubscribe();
+    };
+
+    fetchData();
   }, []);
 
-
-
   const handleUpdate = async () => {
+    setIsLoading(true);
     try {
-      const auth = getAuth();
-        const user = auth.currentUser;
-        if (!user) {
-          throw new Error('User is not authenticated');
-        }
-        const idToken = await user.getIdToken();
-       
-        
-      setIsLoading(true);
-
-        const apiUrl = import.meta.env.VITE_APP_API_URL;
-        const response = await axios.get(
-          `${apiUrl}/devices/plant-devices?plantId=35`,
-          // "http://127.0.0.1:5000/devices/plant-devices?plantId=35",
-          {
-            method: 'GET',
-            headers: {
-              "Authorization": `Bearer ${idToken}`,
-              "Content-Type": "application/json"
-            }
-          });
-    
-        setDevices(response.data.devices);
-        setIsLoading(false);
-
+      const apiUrl = import.meta.env.VITE_APP_API_URL;
+      const data = await authback(`${apiUrl}/devices/plant-devices?plantId=35`);
+      setDevices(data.devices);
     } catch (error) {
       console.error("Error fetching data:", error);
+    } finally {
       setIsLoading(false);
     }
   };
@@ -143,7 +101,6 @@ const PanelUsuarioFinal = () => {
   const deviceImages = [
     "https://www.europe-solarshop.com/media/catalog/product/cache/1/image/600x/17f82f742ffe127f42dca9de82fb58b1/s/m/sma_sunny_tripower_6000tl_20.png",
     "https://i.ebayimg.com/images/g/GH4AAOSwPJlkeiH5/s-l1600.webp",
-    // ... y así sucesivamente para cada dispositivo
   ];
 
   return (
@@ -152,6 +109,30 @@ const PanelUsuarioFinal = () => {
         {isLoading ? (
           <div className="flex justify-center items-center h-64">
             <ApiLoader />
+          </div>
+        ) : userError ? ( // Verifica si hay un error de usuario
+          <div
+            className="flex items-center justify-center min-h-screen"
+            style={{
+              backgroundImage:
+                "url('https://w7.pngwing.com/pngs/702/783/png-transparent-high-voltage-sign-symbol-hazardous-dangerous-electricity-electrical-above-threshold.png')",
+              filter: "brightness(1.2)",
+              mixBlendMode: "multiply",
+              backgroundSize: "40%",
+              backgroundPosition: "center", // Centra la imagen en el div
+              backgroundRepeat: "no-repeat", // Asegura que la imagen no se repita
+            }}
+          >
+            <div className="flex flex-col jsutify-center align-cente text-center m-8">
+              <h2 className="text-lg text-blue-900 bg-white p-4 rounded-md shadow-md">
+                You have to login to access at this section.
+              </h2>
+              <NavLink to="/">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold mt-8 py-2 px-4 rounded">
+                  Go to login
+                </button>
+              </NavLink>
+            </div>
           </div>
         ) : (
           <>
@@ -165,6 +146,7 @@ const PanelUsuarioFinal = () => {
                 {typeEffect}
               </span>
             </div>
+
             <div className="flex flex-col items-center justify-center mb-6">
               <h1 className="text-2xl md:text-3xl text-gray-800 my-4">
                 Your Devices
@@ -172,7 +154,11 @@ const PanelUsuarioFinal = () => {
               <div className="flex space-x-4">
                 <NavLink to="/home">
                   <button>
-                    <img className="w-10 h-10 rounded-full" src={arrow} alt="Back" />
+                    <img
+                      className="w-10 h-10 rounded-full"
+                      src={arrow}
+                      alt="Back"
+                    />
                   </button>
                 </NavLink>
                 <button
@@ -184,116 +170,118 @@ const PanelUsuarioFinal = () => {
               </div>
             </div>
             <div className="flex items-center justify-start">
-    <NavLink to="/deviceReg" className='flex items-center ml-24'>
-        <span>Add new device</span>
-        <button className="ml-4">
-            <IoIosAddCircle className="w-8 h-8" />
-        </button>
-    </NavLink>
-</div>
+              <NavLink to="/deviceReg" className="flex items-center ml-24">
+                <span>Add new device</span>
+                <button className="ml-4">
+                  <IoIosAddCircle className="w-8 h-8" />
+                </button>
+              </NavLink>
+            </div>
 
-            
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 m-20">
-  {devices.map((device, index) => (
-    <div
-      key={index}
-      className="border rounded-lg bg-white shadow-md p-4 cursor-pointer relative"
-      onClick={() => openModal(device)}
-    >
-      {/* Imagen de SMA en el borde superior derecho */}
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Logo_SMA.svg/600px-Logo_SMA.svg.png"
-        alt="SMA Logo"
-        className="absolute top-2 right-2 w-12 h-auto"
-      />
+              {devices.map((device, index) => (
+                <div
+                  key={index}
+                  className="border rounded-lg bg-white shadow-md p-4 cursor-pointer relative"
+                  onClick={() => openModal(device)}
+                >
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Logo_SMA.svg/600px-Logo_SMA.svg.png"
+                    alt="SMA Logo"
+                    className="absolute top-2 right-2 w-12 h-auto"
+                  />
+                  <div className="flex items-center space-x-4">
+                    <img
+                      src={deviceImages[index % deviceImages.length]}
+                      alt={`Imagen del dispositivo ${device.name}`}
+                      className="w-16 h-16 object-cover rounded-full"
+                    />
+                    <div>
+                      <h3 className="text-lg font-semibold text-gray-800">
+                        {device.name}
+                      </h3>
+                      <p className="text-sm text-gray-600">
+                        Type: {device.type}
+                      </p>
+                      <p className="text-sm text-gray-600">
+                        Vendor: {device.vendor}
+                      </p>
+                    </div>
+                  </div>
 
-      {/* Contenido principal de la tarjeta */}
-      <div className="flex items-center space-x-4">
-        <img
-          src={deviceImages[index % deviceImages.length]}
-          alt={`Imagen del dispositivo ${device.name}`}
-          className="w-16 h-16 object-cover rounded-full"
-        />
-        <div>
-          <h3 className="text-lg font-semibold text-gray-800">
-            {device.name}
-          </h3>
-          <p className="text-sm text-gray-600">
-            Type: {device.type}
-          </p>
-          <p className="text-sm text-gray-600">
-            Vendor: {device.vendor}
-          </p>
-        </div>
-      </div>
-
-      <div className="mt-4 text-gray-600">
-        <p>Model: {device.product}</p>
-        <p>Serial: {device.serial || "None"}</p>
-        <p>Voltage: {device.generatorPower || "None"}</p>
-        <p>Timezone: {device.timezone}</p>
-        <p
-          className={`font-semibold ${
-            device.isActive ? "text-blue-600" : "text-gray-400"
-          }`}
-        >
-          {device.isActive ? "Connected" : "Disconnected"}
-        </p>
-      </div>
-    </div>
-  ))}
-</div>
+                  <div className="mt-4 text-gray-600">
+                    <p>Model: {device.product}</p>
+                    <p>Serial: {device.serial || "None"}</p>
+                    <p>Voltage: {device.generatorPower || "None"}</p>
+                    <p>Timezone: {device.timezone}</p>
+                    <p
+                      className={`font-semibold ${
+                        device.isActive ? "text-blue-600" : "text-gray-400"
+                      }`}
+                    >
+                      {device.isActive ? "Connected" : "Disconnected"}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
           </>
         )}
 
-{isModalOpen && selectedDevice && (
-  <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
-    <div className="bg-blue-100 p-6 rounded-lg shadow-lg w-3/4 md:w-1/2 lg:w-1/3 relative">
-      {/* Imagen de SMA en el borde superior derecho del modal */}
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Logo_SMA.svg/600px-Logo_SMA.svg.png"
-        alt="SMA Logo"
-        className="absolute top-2 right-2 w-12 h-auto"
-      />
+        {isModalOpen && selectedDevice && (
+          <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
+            <div className="bg-blue-100 p-6 rounded-lg shadow-lg w-3/4 md:w-1/2 lg:w-1/3 relative">
+              {/* Imagen de SMA en el borde superior derecho del modal */}
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Logo_SMA.svg/600px-Logo_SMA.svg.png"
+                alt="SMA Logo"
+                className="absolute top-2 right-2 w-12 h-auto"
+              />
 
-      <div className="flex items-center space-x-4">
-        <img
-          src={deviceImages[selectedDevice.deviceId % deviceImages.length]}
-          alt={`Imagen del dispositivo ${selectedDevice.name}`}
-          className="w-16 h-16 object-cover rounded-full"
-        />
-        <div>
-          <h3 className="text-lg font-semibold text-gray-800">
-            {selectedDevice.name}
-          </h3>
-          <p className="text-sm text-gray-600">Type: {selectedDevice.type}</p>
-          <p className="text-sm text-gray-600">Vendor: {selectedDevice.vendor}</p>
-        </div>
-      </div>
+              <div className="flex items-center space-x-4">
+                <img
+                  src={
+                    deviceImages[selectedDevice.deviceId % deviceImages.length]
+                  }
+                  alt={`Imagen del dispositivo ${selectedDevice.name}`}
+                  className="w-16 h-16 object-cover rounded-full"
+                />
+                <div>
+                  <h3 className="text-lg font-semibold text-gray-800">
+                    {selectedDevice.name}
+                  </h3>
+                  <p className="text-sm text-gray-600">
+                    Type: {selectedDevice.type}
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    Vendor: {selectedDevice.vendor}
+                  </p>
+                </div>
+              </div>
 
-      <div className="mt-4 text-gray-600">
-        <p>Model: {selectedDevice.product}</p>
-        <p>Serial: {selectedDevice.serial || "None"}</p>
-        <p>Voltage: {selectedDevice.generatorPower || "None"}</p>
-        <p>Timezone: {selectedDevice.timezone}</p>
-        <p
-          className={`font-semibold ${
-            selectedDevice.isActive ? "text-blue-600" : "text-gray-400"
-          }`}
-        >
-          {selectedDevice.isActive ? "Connected" : "Disconnected"}
-        </p>
-      </div>
+              <div className="mt-4 text-gray-600">
+                <p>Model: {selectedDevice.product}</p>
+                <p>Serial: {selectedDevice.serial || "None"}</p>
+                <p>Voltage: {selectedDevice.generatorPower || "None"}</p>
+                <p>Timezone: {selectedDevice.timezone}</p>
+                <p
+                  className={`font-semibold ${
+                    selectedDevice.isActive ? "text-blue-600" : "text-gray-400"
+                  }`}
+                >
+                  {selectedDevice.isActive ? "Connected" : "Disconnected"}
+                </p>
+              </div>
 
-      <button
-        onClick={closeModal}
-        className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
-      >
-        Close
-      </button>
-    </div>
-  </div>
-)}
+              <button
+                onClick={closeModal}
+                className="mt-4 bg-red-500 text-white px-4 py-2 rounded"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
--- gaia_server/src/middlewares.py
+++ gaia_server/src/middlewares.py
@@ -1,23 +1,26 @@
 from functools import wraps
 from flask import request, jsonify
-import firebase_admin
+
 from firebase_admin import auth
 
 def require_firebase_auth(f):
     @wraps(f)
     def middleware_function(*args, **kwargs):
         print("Verificando token...") 
+        # Obtener el token JWT desde los headers de la solicitud
         idToken = request.headers.get('Authorization')
         print(f"Token recibido: {idToken}")
         if not idToken or not idToken.startswith('Bearer '):
             return jsonify({'message': 'Authorization token is missing or invalid'}), 401
         
         try:
+            # Extraer el token JWT sin el prefijo "Bearer "
             idToken = idToken.split('Bearer ')[1]
+            # Verificar el token JWT con Firebase Admin SDK
             decoded_token = auth.verify_id_token(idToken)
             print(decoded_token)
             uid = decoded_token['uid']
-
+        
         except Exception as e:
 
             return jsonify({'message': 'Unauthorized', 'error': str(e)}), 401
