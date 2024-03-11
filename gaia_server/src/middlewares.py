b5a9a37ad65b681354c85dd608ffd5812192f568
agusNazer <agustin.nazer@hotmail.com>
2024-03-11 12:15:07 -0300

    middleware back

--- gaia_server/src/middlewares.py
+++ gaia_server/src/middlewares.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from flask import request, jsonify
-
+import firebase_admin
 from firebase_admin import auth
 
 def require_firebase_auth(f):
@@ -18,11 +18,16 @@ def require_firebase_auth(f):
             idToken = idToken.split('Bearer ')[1]
             # Verificar el token JWT con Firebase Admin SDK
             decoded_token = auth.verify_id_token(idToken)
-            print(decoded_token)
-            uid = decoded_token['uid']
-        
+            print(f"Token decodificado: {decoded_token}")
+            # Aquí podrías establecer información del usuario en el contexto de la solicitud si es necesario
+            # Por ejemplo: request.current_user = decoded_token
+        except firebase_admin.exceptions.FirebaseError as firebase_error:
+            # Manejo específico de errores de Firebase
+            print(f"Error de Firebase al verificar el token: {firebase_error}")
+            return jsonify({'message': 'Unauthorized', 'error': str(firebase_error)}), 401
         except Exception as e:
-
+            # Manejo de otros errores inesperados
+            print(f"Error general al verificar el token: {e}")
             return jsonify({'message': 'Unauthorized', 'error': str(e)}), 401
         
         # Si el token es válido, continuar con la ejecución de la función original
