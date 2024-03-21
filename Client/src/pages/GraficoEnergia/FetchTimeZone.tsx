7afe8e7389f908f0c68d9c519f4bed72e5f2b06e
agusNazer <agustin.nazer@hotmail.com>
2024-03-21 00:44:44 -0300

    fix:grafico energia/intensity energy

---
+++ Client/src/pages/GraficoEnergia/FetchTimeZone.tsx
@@ -0,0 +1,19 @@
+
+export interface TimeZoneApiResponse {
+    datetime: string;
+    timezone: string;
+    utc_offset: string
+}
+
+export const fetchTimeZoneInfo = async (timezone:string): Promise<TimeZoneApiResponse | null> => {
+    const url = `http://worldtimeapi.org/api/timezone/${timezone}`;
+    try {
+        const response = await fetch(url);
+        const data: TimeZoneApiResponse = await response.json();
+        return data;
+    } catch (error) {
+        console.log("Error fetching time zone information", error);
+        return null;
+    }
+};
+
